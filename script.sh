6f95cf7aec37e5acc1e1e4c1df4ba392cb5cd6e2
jabberwockyy <frida.bolanos@smartla.net>
2019-05-05 21:16:07 -0600

    mas opciones

--- script.sh
+++ script.sh
@@ -5,14 +5,50 @@
 
 clear
 s=0
+	echo -n "Bienvenido potatoOS, ingresa 'do' y un comando"
+	echo "Accion 1: ac1
+		  Accion 2: ac2
+		  Apagar : byebye
+		  Reboot : isalive"
 while [[ $s -eq 0 ]]; do
-	echo -n "ingrese un numero"
+	ech
 	read n1
-	if [[ $n1 -eq 0 ]]; then
-		echo -n "continua el ciclo"
-	else
-		echo -n "adios"
-		s=1
-	fi
+	case $n1 in
+		"do ac1" )
+			echo "Accion1"
+			;;
+		"do ac2" )
+			echo "Accion2"
+			;;
+		"do byebye" )
+			echo "El sistema se apagara, byebye"
+			s=1
+			;;
+		"do isalive" )
+			echo "El sistema se reiniciara, isalive?"
+			s=2
+			;;
+		"do" )
+			echo "Debe ingresar una accion despues del comando"
+			;;
+		* )
+			echo "::WARNING:: El comando ingresado no existe"
+			;;
+
+	esac
+	# if [[ $n1 -eq 0 ]]; then
+	# 	echo -n "continua el ciclo"
+	# else
+	# 	echo -n "adios"
+	# 	s=1
+	# fi
 done
+case $s in
+	1 )
+	sudo shutdown -h now
+		;;
+	2 )
+	sudo reboot
+		;;
+esac
 
