05ca57bcead233a9b8862703d2c693719e0a95e0
jabberwockyy <frida.bolanos@smartla.net>
2019-05-07 11:07:27 -0600

    new options

--- script.sh
+++ script.sh
@@ -1,60 +1,128 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
-#display /home/mariale/índice.jpeg
-#echo "hola mundo"
 
 clear
+
 trap '' 2
+
 s=0
-	echo "Bienvenido potatoOS, ingresa 'do ' y un comando"
-	echo "		Accion 1: ac1
-		  Accion 2: ac2
-		  Apagar : byebye
-		  Reboot : isalive"
+	
+echo "Bienvenido potatoOS, ingresa 'do ' y un comando"
+	
+
+echo "	
+
+	Calculadora: calcular
+		  
+	Fecha y Hora: f-h
+		  	
+	Apagar : byebye
+		  
+	Reboot : isalive"
+
+
 while [[ $s -eq 0 ]]; do
+	
 	echo -n "$ "
+	
 	read n1
+	
 	case $n1 in
-		"do ac1" )
-			echo "Accion1"
+		
+		"do calcular" )
+						
+			echo -n "ingresar un numero: "
+		
+			read n2
+		
+			echo -n "ingrese otro numero: "
+		
+			read n3
+		
+			P=$(expr $n2 + $n3)
+		
+			echo "El resultado de la suma es: " $P
+		
+			R=$(expr $n2 - $n3)
+		
+			echo "El resultado de la resta es: " $R
+		
+			M=$(expr $n2 \* $n3)
+		
+			echo "El resultado de la multiplicacion es: " $M
+		
+			D=$(expr $n2 \/ $n3)
+		
+			echo "El resultado de division es: " $D 
+		
+			
 			;;
-		"do ac2" )
-			echo "Accion2"
+		
+		"do f-h" )
+				
+			dia=`date +"%d/%m/%Y"`
+			hora=`date +"%H:%M"`
+
+			echo "Hoy es $dia y la hora actual es $hora"
+		
 			;;
+	
+	
 		"do byebye" )
+	
 			echo "El sistema se apagara, byebye"
+	
 			s=1
+			
 			;;
+		
 		"do isalive" )
+		
 			echo "El sistema se reiniciara, isalive?"
+			
 			s=2
+			
 			;;
+		
 		"do" )
+			
 			echo "Debe ingresar una accion despues del comando"
+		
 			;;
+		
 		"papaya" )
+		
 			echo "winter is here..."
+
 			s=3
+			
 			;;
+		
+	
 		* )
+			
 			echo "::WARNING:: El comando ingresado no existe"
+			
 			;;
 
+	
+	
 	esac
-	# if [[ $n1 -eq 0 ]]; then
-	# 	echo -n "continua el ciclo"
-	# else
-	# 	echo -n "adios"
-	# 	s=1
-	# fi
-done
-trap – 2
+	
+
+done 
+trap  2
+
 case $s in
+	
 	1 )
 	sudo shutdown -h now
-		;;
+		
+			;;
+	
 	2 )
 	sudo reboot
-		;;
-esac
+		
+			;;
 
+esac
